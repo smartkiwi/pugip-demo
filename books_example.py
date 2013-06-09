87b6ae8c10a42739b9dd75dbd20760f197eed497
vvlad <smartkiwi@gmail.com>
2013-06-09 16:25:20 -0400

    select query generation example

--- books_example.py
+++ books_example.py
@@ -3,15 +3,15 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative.api import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.session import Session
-from sqlalchemy.schema import MetaData, Column, ForeignKey
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.sql.expression import case
+from sqlalchemy.sql.functions import count
 from sqlalchemy.types import Integer, String, DateTime
 
 __author__ = 'vvlad'
 
 
 Base = declarative_base()
-
-
 class Book(Base):
     __tablename__ = 'book'
     id = Column(Integer, primary_key=True)
@@ -24,28 +24,50 @@ class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    birth_date = Column(DateTime)
     books = relationship(Book, backref="author")
 
 
 db_uri = "sqlite:///"
-
-engine = create_engine(db_uri, echo=True)
+engine = create_engine(db_uri, echo=False)
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
-session = Session(bind=engine)
 
-book = Book()
-book.title = 'Essential SQLAlchemy'
-book.publication_date = datetime.datetime.strptime('2010-05-05','%Y-%m-%d')
-book.pages_count = 240
+
+insert_stmt = Book.__table__.insert()
+print insert_stmt
+data = {'title': 'Essential SQLAlchemy', 'publication_date': datetime.datetime.strptime('2010-05-05', '%Y-%m-%d'),
+        'pages_count': 240}
+engine.connect().execute(insert_stmt, data)
+
+
+session = Session(bind=engine)
+q = session.query(Book).filter(Book.title == 'Essential SQLAlchemy')
+print q
+book = q.one()
+print (book.id, book.title)
+
 
 author = Author(name='Rick Copeland')
 author.books.append(book)
 session.add(book)
-
 session.flush()
+
+####
+# select CASE WHEN (BOOK.pages_count > 200) THEN 1 ELSE 0 END is_novel, count(*)
+# from BOOK
+# group by CASE WHEN (BOOK.pages_count > 200) THEN 1 ELSE 0 END
+# order by CASE WHEN (BOOK.pages_count > 200) THEN 1 ELSE 0 END
+#
+is_novel_column = case([(Book.pages_count>200, 1)], else_=0)
+novel_query = session.query(is_novel_column.label('is_alias'), count()).\
+    group_by(is_novel_column).\
+    order_by(is_novel_column)
+
+print novel_query
+print novel_query.all()
+
+
 session.close()
 
 
